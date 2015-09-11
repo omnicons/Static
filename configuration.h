ce1ea241a205a8d07dce65f9c0e0a8194100829a
AppleDash <derp@derpy.fr>
2015-09-11 04:02:27 -0400

    Document configuration class.

--- configuration.h
+++ configuration.h
@@ -9,8 +9,15 @@ using std::cout;
 class Configuration {
     public:
         Configuration(std::string path);
+        /* Parse the configuration. This must be called before you try to get anything from it. */
         bool Parse();
+        /* Get a string from the configuration, by key. Returns true and places it into out if it was found. 
+         * Returns false if not found.
+         */
         bool GetString(std::string key, std::string &out);
+        /* Get an int from the configuration, by key. Returns true and places it into out if it was found and
+         * a valid int. Returns false if it was not found or was not convertable to an int.
+         */
         bool GetInt(std::string, int &out);
     private:
         std::string file;
